83afd659bd37877e5caef44c7aa7f9f73435759b
Mahmoud Abdulshakour <mahmoud.ali1842001@gmail.com>
2024-02-20 23:29:05 +0200

    Add review repo

---
+++ Repos/Contracts/IReviewRepo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using goodreads.Models;
+
+namespace goodreads.Repos.Contracts
+{
+    public interface IReviewRepo
+    {
+        Task<Review> Create(Review review);
+        Task<Review?> GetById(int reviewId);
+        Task<List<Review>> GetBookReviews(int bookId);
+        Task<List<Review>> GetUserReviews(string userId);
+        Task<Review?> Update(Review updatedReview);
+        Task Delete(Review review);
+    }
+}
---
+++ Repos/ReviewRepo.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using goodreads.Database;
+using goodreads.Models;
+using goodreads.Repos.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace goodreads.Repos
+{
+    public class ReviewRepo : IReviewRepo
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReviewRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Review> Create(Review review)
+        {
+            await _context.Reviews.AddAsync(review);
+            await _context.SaveChangesAsync();
+            return review;
+        }
+
+        public async Task Delete(Review review)
+        {
+            _context.Reviews.Remove(review);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Review>> GetBookReviews(int bookId)
+        {
+            return await _context.Reviews.Where(r => r.BookId == bookId).ToListAsync();
+        }
+
+        public async Task<Review?> GetById(int reviewId)
+        {
+            return await _context.Reviews.FindAsync(reviewId);
+        }
+
+        public async Task<List<Review>> GetUserReviews(string userId)
+        {
+            return await _context.Reviews.Where(r => r.AppUserId == userId).ToListAsync();
+        }
+
+        public async Task<Review?> Update(Review updatedReview)
+        {
+            var review = await _context.Reviews.FindAsync(updatedReview.Id);
+            if (review == null)
+                return null;
+
+            review.Content = updatedReview.Content;
+            await _context.SaveChangesAsync();
+
+            return review;
+        }
+    }
+}
